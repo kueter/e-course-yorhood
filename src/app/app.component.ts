630757afccf51911d3cfdbc0eeb8a024ced9b644
roody95 <agbassahr@gmail.com>
2020-12-08 20:26:37 +0000

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,14 @@ export class AppComponent {
   title = 'yorhood';
 
   topics = [
-    { name: '', img: '', course: '' },
-    { name: '', img: '', course: '' },
-    { name: '', img: '', course: '' },
-    { name: '', img: '', course: '' }
+    { name: 'Computer Science', img: '../assets/img/', course: 102 },
+    { name: 'Web Development', img: '../assets/img/', course: 100 },
+    { name: 'Data Science', img: '../assets/img/', course: 298 },
+    { name: 'Engineering', img: '../assets/img/', course: 120 },
+    { name: 'Architecture', img: '../assets/img/', course: 12 },
+    { name: 'Art & Design', img: '../assets/img/', course: 202 },
+    { name: 'Business Study', img: '../assets/img/', course: 103 },
+    { name: 'Medicine', img: '../assets/img/', course: 90 }
   ];
 
 }
