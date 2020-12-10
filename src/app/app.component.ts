4a2197254b22340d7ef49e2a2baac2de372a1648
roody95 <agbassahr@gmail.com>
2020-12-10 12:27:06 +0000

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,14 +6,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-
+      trigger('x-x0', [
+        state('left', style({  transform: 'translateX(0%)'})),
+        state('right', style({ transform: 'translateX(235%)'})),
+        transition('left => right', animate('1s ease-in-out', keyframes([
+          style({ transform: 'translateX(235%)'})
+        ]))),
+        transition('right => left', animate('1s ease-in-out', keyframes([
+          style({ transform: 'translateX(0%)'})
+        ])))
+      ])
   ]
 })
 export class AppComponent {
   title = 'yorhood';
 
-  switchTo() {
+  state = false;
 
+  switchTo() {
+    this.state = !this.state;
   }
 
 }
