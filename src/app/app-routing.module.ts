4a76e9e6b7e6c75a790969fdc9f7fc0710d09674
roody95 <agbassahr@gmail.com>
2020-12-11 17:40:33 +0000

    com

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
 import { CoursesComponent } from './courses/courses.component';
 import { HomeComponent } from './home/home.component';
 
@@ -8,10 +9,9 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch:'full' },
   { path: 'courses', component: CoursesComponent },
-  { path: 'projects', component: HomeComponent },
   { path: 'contact', component: HomeComponent },
   { path: 'about', component: HomeComponent },
-  { path: 'blog', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
 ];
 
 @NgModule({
