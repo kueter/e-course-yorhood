721dd10d2f0797cabd4de0ba311309bbf29ff050
roody95 <agbassahr@gmail.com>
2020-12-12 08:47:10 +0000

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,34 @@
 import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component, HostListener, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-      trigger('x-x0', [
-        state('left', style({  transform: 'translateX(0%)'})),
-        state('right', style({ transform: 'translateX(235%)'})),
-        transition('left => right', animate('1s ease-in-out', keyframes([
-          style({ transform: 'translateX(235%)'})
-        ]))),
-        transition('right => left', animate('1s ease-in-out', keyframes([
-          style({ transform: 'translateX(0%)'})
-        ])))
-      ]),
+    trigger('x-x0', [
+      state('left', style({ transform: 'translateX(0%)' })),
+      state('right', style({ transform: 'translateX(235%)' })),
+      transition('left => right', animate('1s ease-in-out', keyframes([
+        style({ transform: 'translateX(235%)' })
+      ]))),
+      transition('right => left', animate('1s ease-in-out', keyframes([
+        style({ transform: 'translateX(0%)' })
+      ])))
+    ]),
 
-      trigger('x-y0', [
-        state('right', style({  transform: 'translateX(0%)'})),
-        state('left', style({ transform: 'translateX(-42%)'})),
-        transition('right => left', animate('1s ease-in-out', keyframes([
-          style({ transform: 'translateX(-42%)'})
-        ]))),
-        transition('left => right', animate('1s ease-in-out', keyframes([
-          style({ transform: 'translateX(0%)'})
-        ])))
-      ])
+    trigger('x-y0', [
+      state('right', style({ transform: 'translateX(0%)' })),
+      state('left', style({ transform: 'translateX(-42%)' })),
+      transition('right => left', animate('1s ease-in-out', keyframes([
+        style({ transform: 'translateX(-42%)' })
+      ]))),
+      transition('left => right', animate('1s ease-in-out', keyframes([
+        style({ transform: 'translateX(0%)' })
+      ])))
+    ])
   ]
 })
 export class AppComponent implements OnInit {
@@ -40,8 +42,8 @@ export class AppComponent implements OnInit {
 
 
   ngOnInit(): void {
-       setTimeout(() => {
-        this.boot = true;
+    setTimeout(() => {
+      this.boot = true;
     }, 500);
   }
 
@@ -53,15 +55,21 @@ export class AppComponent implements OnInit {
 
   @HostListener('window:scroll', [])
   onScroll() {
-    if(document.documentElement.scrollTop >= 47) {
-        this.fixed = true;
+    if (document.documentElement.scrollTop >= 47) {
+      this.fixed = true;
     }
 
-    if(document.documentElement.scrollTop < 47 ) {
-        this.fixed = false;
+    if (document.documentElement.scrollTop < 47) {
+      this.fixed = false;
     }
   }
 
-
+  basket() {
+    $(".basket").fadeIn()
+      .css({ top: 0, position: 'fixed' })
+      .animate({ height: '600px' }, 800, function () {
+        //callback
+      });
+  }
 
 }
