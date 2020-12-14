fcc759c720a0cf507e1747496edd56cba30f7a54
roody95 <agbassahr@gmail.com>
2020-12-14 20:09:26 +0000

    vv

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,17 +7,20 @@ import { AppService } from '../app.service';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-  shoppingCartItems;
+  shoppingCartItems = [];
+  total;
 
   constructor(private service: AppService) { }
 
   ngOnInit(): void {
-    this.service.getItems().subscribe((_ )=> {
-      this.shoppingCartItems = _;
-      console.log(_);
-    });
+    // this.service.getItems().subscribe((_ )=> {
+    //   this.shoppingCartItems = _;
+    //   console.log(_);
+    // });
 
+    this.shoppingCartItems.push( { name: 'Marketing online', instructor: 'Maitre Gims', lesson: 20, joined: 211, cost: '150', uptime: '2019-09-01', mark: 4, status: 'Available', remise: '', preload: '../../assets/img/preload.png', category: 'Business Study', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, consequatur in at molestias accusantium officiis facere dolorem placeat nemo veritatis.'},);
 
+    this.service.getTotalAmount().subscribe(_ => this.total = _);
   }
 
 }
